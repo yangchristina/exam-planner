3d8496e1cecd19f6993dfd9a979d41ad0d1de7ea
yangchristina <chryang@students.cs.ubc.ca>
2023-04-19 14:09:53 -0700

    add border

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import UnitButton from './UnitButton'
 import CourseUnitNav from './CourseUnitNav'
 import CourseHeader from './CourseHeader'
 import ProgressBar from './ProgressBar'
 
 const Course = () => {
     return (
-        <div className="w-96 h-96 mx-auto bg-white rounded-xl shadow-lg flex flex-col items-stretch" >
+        <div className="w-96 h-96 mx-auto bg-white rounded-xl shadow-lg flex flex-col items-stretch border-2" >
             <CourseHeader />
             <CourseUnitNav />
             <ProgressBar />
